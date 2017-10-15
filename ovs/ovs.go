f5246a12c39330c0c91bfe523c4e36069a73fea7
John-Lin <linton.tw@gmail.com>
2017-10-15 13:33:31 +0800

    add bridge as a default gateway and has a Ip

--- ovs/ovs.go
+++ ovs/ovs.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"net"
 	"runtime"
+	"syscall"
 
 	"github.com/containernetworking/cni/pkg/skel"
 	"github.com/containernetworking/cni/pkg/types"
@@ -14,18 +15,29 @@ import (
 	"github.com/containernetworking/plugins/pkg/ip"
 	"github.com/containernetworking/plugins/pkg/ipam"
 	"github.com/containernetworking/plugins/pkg/ns"
+
 	"github.com/j-keck/arping"
 	log "github.com/sirupsen/logrus"
+	"github.com/vishvananda/netlink"
 )
 
 const defaultBrName = "br0"
 
 type NetConf struct {
 	types.NetConf
-	IsMaster   bool     `json:"isMaster"`
-	OVSBrName  string   `json:"ovsBridge"`
-	VtepIPs    []string `json:"vtepIPs"`
-	Controller string   `json:"controller,omitempty"`
+	IsMaster    bool     `json:"isMaster"`
+	OVSBrName   string   `json:"ovsBridge"`
+	IsGW        bool     `json:"isGateway"`
+	IsDefaultGW bool     `json:"isDefaultGateway"`
+	IPMasq      bool     `json:"ipMasq"`
+	VtepIPs     []string `json:"vtepIPs"`
+	Controller  string   `json:"controller,omitempty"`
+}
+
+type gwInfo struct {
+	gws               []net.IPNet
+	family            int
+	defaultRouteFound bool
 }
 
 func init() {
@@ -43,6 +55,108 @@ func loadNetConf(bytes []byte) (*NetConf, string, error) {
 		return nil, "", fmt.Errorf("failed to load netconf: %v", err)
 	}
 	return n, n.CNIVersion, nil
+}
+
+// calcGateways processes the results from the IPAM plugin and does the
+// following for each IP family:
+//    - Calculates and compiles a list of gateway addresses
+//    - Adds a default route if needed
+func calcGateways(result *current.Result, n *NetConf) (*gwInfo, *gwInfo, error) {
+
+	gwsV4 := &gwInfo{}
+	gwsV6 := &gwInfo{}
+
+	for _, ipc := range result.IPs {
+
+		// Determine if this config is IPv4 or IPv6
+		var gws *gwInfo
+		defaultNet := &net.IPNet{}
+		switch {
+		case ipc.Address.IP.To4() != nil:
+			gws = gwsV4
+			gws.family = netlink.FAMILY_V4
+			defaultNet.IP = net.IPv4zero
+		case len(ipc.Address.IP) == net.IPv6len:
+			gws = gwsV6
+			gws.family = netlink.FAMILY_V6
+			defaultNet.IP = net.IPv6zero
+		default:
+			return nil, nil, fmt.Errorf("Unknown IP object: %v", ipc)
+		}
+		defaultNet.Mask = net.IPMask(defaultNet.IP)
+
+		// All IPs currently refer to the container interface
+		// Add the IP to the interface
+		// 0 -> bridge itself
+		// 1 -> veth endpoint
+		// 2 -> interface in container
+		ipc.Interface = current.Int(2)
+
+		// If not provided, calculate the gateway address corresponding
+		// to the selected IP address
+		if ipc.Gateway == nil && n.IsGW {
+			ipc.Gateway = calcGatewayIP(&ipc.Address)
+		}
+
+		// Add a default route for this family using the current
+		// gateway address if necessary.
+		if n.IsDefaultGW && !gws.defaultRouteFound {
+			for _, route := range result.Routes {
+				if route.GW != nil && defaultNet.String() == route.Dst.String() {
+					gws.defaultRouteFound = true
+					break
+				}
+			}
+			if !gws.defaultRouteFound {
+				result.Routes = append(
+					result.Routes,
+					&types.Route{Dst: *defaultNet, GW: ipc.Gateway},
+				)
+				gws.defaultRouteFound = true
+			}
+		}
+
+		// Append this gateway address to the list of gateways
+		if n.IsGW {
+			gw := net.IPNet{
+				IP:   ipc.Gateway,
+				Mask: ipc.Address.Mask,
+			}
+			gws.gws = append(gws.gws, gw)
+		}
+	}
+	return gwsV4, gwsV6, nil
+}
+
+func ensureBridgeAddr(br *OVSSwitch, family int, ipn *net.IPNet) error {
+	ovsbrLink, err := netlink.LinkByName(br.BridgeName)
+	if err != nil {
+		return fmt.Errorf("could not get ovs bridge link: %v", err)
+	}
+
+	addrs, err := netlink.AddrList(ovsbrLink, family)
+	if err != nil && err != syscall.ENOENT {
+		return fmt.Errorf("could not get list of IP addresses: %v", err)
+	}
+
+	ipnStr := ipn.String()
+	for _, a := range addrs {
+		// string comp is actually easiest for doing IPNet comps
+		if a.IPNet.String() == ipnStr {
+			return nil
+		}
+	}
+
+	addr := &netlink.Addr{IPNet: ipn, Label: ""}
+	if err := netlink.AddrAdd(ovsbrLink, addr); err != nil {
+		return fmt.Errorf("could not add IP address to %q: %v", br.BridgeName, err)
+	}
+
+	// enables the link device
+	if err := netlink.LinkSetUp(ovsbrLink); err != nil {
+		return fmt.Errorf("could not set ovs bridge link up: %v", err)
+	}
+	return nil
 }
 
 func ensureBridge(brName string) (*OVSSwitch, error) {
@@ -89,6 +203,11 @@ func setupVeth(netns ns.NetNS, br *OVSSwitch, ifName string, mtu int) (*current.
 	return hostIface, contIface, nil
 }
 
+func calcGatewayIP(ipn *net.IPNet) net.IP {
+	nid := ipn.IP.Mask(ipn.Mask)
+	return ip.NextIP(nid)
+}
+
 func setupBridge(n *NetConf) (*OVSSwitch, *current.Interface, error) {
 	// create bridge if necessary
 	ovsbr, err := ensureBridge(n.OVSBrName)
@@ -101,10 +220,21 @@ func setupBridge(n *NetConf) (*OVSSwitch, *current.Interface, error) {
 	}, nil
 }
 
+func enableIPForward(family int) error {
+	if family == netlink.FAMILY_V4 {
+		return ip.EnableIP4Forward()
+	}
+	return ip.EnableIP6Forward()
+}
+
 func cmdAdd(args *skel.CmdArgs) error {
 	n, cniVersion, err := loadNetConf(args.StdinData)
 	if err != nil {
 		return err
+	}
+
+	if n.IsDefaultGW {
+		n.IsGW = true
 	}
 
 	// Create a Open vSwitch bridge
@@ -119,7 +249,7 @@ func cmdAdd(args *skel.CmdArgs) error {
 	}
 	defer netns.Close()
 
-	hostInterface, containerInterface, err := setupVeth(netns, br, args.IfName, 1500)
+	hostInterface, containerInterface, err := setupVeth(netns, br, args.IfName, 1400)
 	if err != nil {
 		return err
 	}
@@ -142,6 +272,12 @@ func cmdAdd(args *skel.CmdArgs) error {
 
 	result.Interfaces = []*current.Interface{brInterface, hostInterface, containerInterface}
 
+	// Gather gateway information for each IP family
+	gwsV4, gwsV6, err := calcGateways(result, n)
+	if err != nil {
+		return err
+	}
+
 	// Configure the container hardware address and IP address(es)
 	if err := netns.Do(func(_ ns.NetNS) error {
 		contVeth, err := net.InterfaceByName(args.IfName)
@@ -149,14 +285,6 @@ func cmdAdd(args *skel.CmdArgs) error {
 			return err
 		}
 
-		// Add the IP to the interface
-		for _, ipc := range result.IPs {
-			// All IPs currently refer to the container interface
-			// 0 -> bridge itself
-			// 1 -> veth endpoint
-			// 2 -> interface in Container.
-			ipc.Interface = current.Int(2)
-		}
 		if err := ipam.ConfigureIface(args.IfName, result); err != nil {
 			return err
 		}
@@ -172,6 +300,29 @@ func cmdAdd(args *skel.CmdArgs) error {
 		return err
 	}
 
+	if n.IsGW {
+		var firstV4Addr net.IP
+		// Set the IP address(es) on the bridge and enable forwarding
+		for _, gws := range []*gwInfo{gwsV4, gwsV6} {
+			for _, gw := range gws.gws {
+				if gw.IP.To4() != nil && firstV4Addr == nil {
+					firstV4Addr = gw.IP
+				}
+
+				err = ensureBridgeAddr(br, gws.family, &gw)
+				if err != nil {
+					return fmt.Errorf("failed to set bridge addr: %v", err)
+				}
+			}
+
+			if gws.gws != nil {
+				if err = enableIPForward(gws.family); err != nil {
+					return fmt.Errorf("failed to enable forwarding: %v", err)
+				}
+			}
+		}
+	}
+
 	if len(n.VtepIPs) != 0 {
 		// Create VxLAN tunnelings
 		if err = br.AddVTEPs(n.VtepIPs); err != nil {
@@ -185,6 +336,16 @@ func cmdAdd(args *skel.CmdArgs) error {
 			return err
 		}
 	}
+
+	// if n.IPMasq {
+	// 	chain := utils.FormatChainName(n.Name, args.ContainerID)
+	// 	comment := utils.FormatComment(n.Name, args.ContainerID)
+	// 	for _, ipc := range result.IPs {
+	// 		if err = ip.SetupIPMasq(ip.Network(&ipc.Address), chain, comment); err != nil {
+	// 			return err
+	// 		}
+	// 	}
+	// }
 
 	return types.PrintResult(result, cniVersion)
 }
