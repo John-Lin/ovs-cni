1c139e448eb9f2bb86429c9c21e66123e4024973
hwchiu <hwchiu@linkernetworks.com>
2017-11-03 10:48:15 +0000

    support subnet chaeck

--- ovs/ipm.go
+++ ovs/ipm.go
@@ -16,8 +16,10 @@ package main
 
 import (
 	"context"
+	"encoding/binary"
 	"fmt"
 	"github.com/coreos/etcd/clientv3"
+	"net"
 	"os"
 	"time"
 )
@@ -31,6 +33,33 @@ type IPMConfig struct {
 }
 
 const etcdPrefix string = "/ovs-cni/networks/"
+
+func powTwo(times int) uint32 {
+	if times == 0 {
+		return uint32(1)
+	}
+
+	var ans uint32
+	ans = 1
+	for i := 0; i < times; i++ {
+		ans *= 2
+	}
+
+	return ans
+}
+
+func ip2int(ip net.IP) uint32 {
+	if len(ip) == 16 {
+		return binary.BigEndian.Uint32(ip[12:16])
+	}
+	return binary.BigEndian.Uint32(ip)
+}
+
+func int2ip(nn uint32) net.IP {
+	ip := make(net.IP, 4)
+	binary.BigEndian.PutUint32(ip, nn)
+	return ip
+}
 
 func checkNodeRegister(nodeName string, cli clientv3.Client) (bool, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
@@ -51,9 +80,20 @@ func checkNodeRegister(nodeName string, cli clientv3.Client) (bool, error) {
 	return true, nil
 }
 
-func checkSubNetRegister(subnet string) error {
+func checkSubNetRegistered(subnet string, cli clientv3.Client) (bool, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	resp, err := cli.Get(ctx, etcdPrefix, clientv3.WithPrefix())
+	cancel()
+	if err != nil {
+		return false, fmt.Errorf("Fetch etcd prefix error:%v", err)
+	}
 
-	return nil
+	for _, ev := range resp.Kvs {
+		if string(ev.Value) == subnet {
+			return true, nil
+		}
+	}
+	return false, nil
 }
 
 func registerSubnet(nodeName string, ipmconfig IPMConfig, cli clientv3.Client) error {
@@ -61,6 +101,26 @@ func registerSubnet(nodeName string, ipmconfig IPMConfig, cli clientv3.Client) e
 	_, err := cli.Put(context.TODO(), etcdPrefix+nodeName, "QQQ")
 	if err != nil {
 		return fmt.Errorf("Put data into etcd fail: %v", err)
+	}
+
+	ipnet := net.ParseIP(ipmconfig.SubnetMin)
+	ipStart := ip2int(ipnet)
+	ipNextSubnet := powTwo(32 - ipmconfig.SubnetLen)
+	ipEnd := net.ParseIP(ipmconfig.SubnetMax)
+
+	fmt.Println(ipEnd)
+	for i := 0; ; i++ {
+		nextSubnet := int2ip(ipStart + ipNextSubnet*uint32(i))
+		success, err := checkSubNetRegistered(nextSubnet.String(), cli)
+		if err != nil {
+			return fmt.Errorf("Check Subnet Exist: %v", err)
+		}
+		if success {
+			return nil
+		}
+		if ipEnd.String() == nextSubnet.String() {
+			return nil
+		}
 	}
 	return nil
 }
@@ -73,15 +133,12 @@ func GetSubnet(ipconfig IPMConfig) error {
 		return fmt.Errorf("Failed to get NodeName: %v", err)
 	}
 
-	fmt.Println(name)
-
 	cli, err := clientv3.New(clientv3.Config{
 		Endpoints:   []string{ipconfig.ETCDUrl},
 		DialTimeout: 5 * time.Second,
 	})
 
 	exist, err := checkNodeRegister(name, *cli)
-
 	if err != nil {
 		fmt.Println(err)
 	}
@@ -89,12 +146,13 @@ func GetSubnet(ipconfig IPMConfig) error {
 	if !exist {
 		registerSubnet(name, ipconfig, *cli)
 	}
+
 	return nil
 }
 
 func main() {
 
-	if err := GetSubnet(IPMConfig{Network: "10.16.0.0", SubnetLen: 24, SubnetMin: "10.16.4.0", SubnetMax: "10.16.2.0", ETCDUrl: "10.240.0.26:2379"}); err != nil {
+	if err := GetSubnet(IPMConfig{Network: "10.16.0.0", SubnetLen: 24, SubnetMin: "10.16.4.0", SubnetMax: "10.16.10.0", ETCDUrl: "10.240.0.26:2379"}); err != nil {
 		fmt.Println(err)
 	}
 }
